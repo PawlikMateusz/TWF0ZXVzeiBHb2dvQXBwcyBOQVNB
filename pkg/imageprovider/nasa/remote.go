730c02ca017e8ef5a6cf80ab31a97bd59ac37652
Mateusz Pawlik <mateusz.pawlik@bshg.com>
2021-03-10 20:59:05 +0100

    Generate request for every date

--- pkg/imageprovider/nasa/remote.go
+++ pkg/imageprovider/nasa/remote.go
@@ -2,6 +2,7 @@ package nasa
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/PawlikMateusz/TWF0ZXVzeiBHb2dvQXBwcyBOQVNB/pkg/client"
@@ -22,6 +23,21 @@ func NewRemoteProvider(baseURL, apiKey string, maxConcurrentRequests int) *Remot
 }
 
 func (rp *RemoteProvider) GetImagesURLs(startDate, endDate time.Time) (urls []string, err error) {
+	req, err := http.NewRequest("GET", rp.baseURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create request: %s", err)
+	}
+	q := req.URL.Query()
+	q.Add("api_key", rp.apiKey)
+	q.Add("date", "")
+	for d := startDate; d.Before(endDate); d = d.AddDate(0, 0, 1) {
+
+		q.Set("date", d.Format("2006-01-02"))
+		req.URL.RawQuery = q.Encode()
+		fmt.Printf("\nMaking request %s", req.URL.String())
+		// resp, err := rp.httpClient.Do(req)
+	}
+
 	return []string{
 		"www.testurl.pl",
 		fmt.Sprint(fmt.Sprint(rp.apiKey)),
